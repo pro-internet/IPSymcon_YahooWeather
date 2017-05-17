589122d19d7a2f65dd6671b4e279095d0db90748
Alex Wellnitz <alex@prointernet.loc>
2017-05-17 16:43:03 +0200

    update getWeatherForcast function

--- YahooWeather_Symcon/module.php
+++ YahooWeather_Symcon/module.php
@@ -33,7 +33,7 @@
             $degree     = $this->ReadPropertyString("Degree");
 
             $BASE_URL = "http://query.yahooapis.com/v1/public/yql";
-            $yql_query = 'select wind from weather.forecast where woeid in (select woeid from geo.places(1) where text="chicago, il")';
+            $yql_query = 'select item.condition from weather.forecast where woeid = 701780';
             $yql_query_url = $BASE_URL . "?q=" . urlencode($yql_query) . "&format=json";
             // Make call with cURL
             $session = curl_init($yql_query_url);
