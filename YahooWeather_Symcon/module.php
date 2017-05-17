e3060a5b64441228d3f388590f542b65f4f30f5f
Alex Wellnitz <alex@prointernet.loc>
2017-05-17 15:58:48 +0200

    change function

--- YahooWeather_Symcon/module.php
+++ YahooWeather_Symcon/module.php
@@ -32,10 +32,17 @@
             $zipcode    = $this->ReadPropertyString("Zipcode");
             $degree     = $this->ReadPropertyString("Degree");
 
-            $result = file_get_contents('http://weather.yahooapis.com/forecastrss?p=55483' . $zipcode . '&u=c' . $degree);
-            $xml = simplexml_load_string($result);
-            //echo htmlspecialchars($result, ENT_QUOTES, 'UTF-8');
-            return $xml;
+            $BASE_URL = "http://query.yahooapis.com/v1/public/yql";
+            $yql_query = 'select wind from weather.forecast where woeid in (select woeid from geo.places(1) where text="chicago, il")';
+            $yql_query_url = $BASE_URL . "?q=" . urlencode($yql_query) . "&format=json";
+            // Make call with cURL
+            $session = curl_init($yql_query_url);
+            curl_setopt($session, CURLOPT_RETURNTRANSFER,true);
+            $json = curl_exec($session);
+            // Convert JSON to PHP object
+            $phpObj =  json_decode($json);
+            var_dump($phpObj);
+            return $phpObj;
         }
     }
 ?>
